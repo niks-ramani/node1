417dd08f05adad3dab0d70de42cdb510f62ab62b
nikunjramani <nikunj.ramani14@gmail.com>
2022-09-26 18:40:11 +0530

    first commit

---
+++ second.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+int main()
+{
+	int i,n,k=0;
+	printf("enter value of n=");
+	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{
+		if(n%i==0)
+		{
+		printf("\t%d",i);
+		k=k+i;
+		}
+	}
+	if(k==n)
+	printf("\nyes %d is perfect number",k);
+	else
+	printf("\n%d is imperfect",n);
+	return 0;
+}
